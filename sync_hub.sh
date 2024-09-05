b109bc4c66fc5b9b8a8b2a11ff87d294333f1225
wángzhìqiáng <zhiqiang2033@gmail.com>
2024-09-05 14:06:59 +0800

    Update sync_hub.sh

--- sync_hub.sh
+++ sync_hub.sh
@@ -2,13 +2,13 @@
 
 set -e
 set -u
-tags=("5.7" "8.0" "8.1" "8.2")
+tags=("5.7" "8.0" "8.1" "8.2" "8.3" "8.4")
 # 循环遍历数组中的每个元素
 for tag in "${tags[@]}"
 do
     echo "Downloading mysql:$tag"
     docker pull mysql:$tag
-    echo "rename mysql:$tag -> registry.cn-hongkong.aliyuncs.com/gfhub/mysql:$tag"
-    docker tag mysql:$tag registry.cn-hongkong.aliyuncs.com/gfhub/mysql:$tag
-    docker push registry.cn-hongkong.aliyuncs.com/gfhub/mysql:$tag
+    echo "rename mysql:$tag -> registry.cn-hongkong.aliyuncs.com/buildx/hub:mysql-$tag"
+    docker tag mysql:$tag registry.cn-hongkong.aliyuncs.com/buildx/hub:mysql-$tag
+    docker push registry.cn-hongkong.aliyuncs.com/buildx/hub:mysql-$tag
 done
